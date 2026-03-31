bbdd46f0e91fc8f4608712d004d50047555790f8
rakesh <raki7445@gmail.com>
2026-03-31 13:15:44 +0530

    Router.route() was implemented for listing and user

--- routes/listing.js
+++ routes/listing.js
@@ -4,49 +4,37 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const {isLoggedin, isOwner,validateListing} = require("../middleware.js");
 const listingController = require("../controllers/listing.js");
 
-//index route
-router.get("/",wrapAsync(listingController.index));
+router
+    .route("/")
+    .get(wrapAsync(listingController.index))
+    .post(
+        isLoggedin,
+        validateListing,
+        wrapAsync(listingController.createListing)
+    );
 
-//new route
-router.get("/new",isLoggedin,listingController.renderNewForm);
+router
+    .route("/new")
+    .get(isLoggedin,listingController.renderNewForm);
 
-//create route
-router.post(
-    "/",
-    isLoggedin,
-    validateListing,
-    wrapAsync(listingController.createListing)
-);
-
-//show route
-router.get(
-    "/:id",
-    wrapAsync(listingController.showListing)
-);
-
-//edit route
-router.get(
-    "/:id/edit",
-    isLoggedin,
-    isOwner,
-    wrapAsync(listingController.renderEditForm)
-);
-
-//update route
-router.put(
-    "/:id",
+router
+    .route("/:id")
+    .get(wrapAsync(listingController.showListing))
+    .put(
     isLoggedin,
     isOwner,
     validateListing,
-    wrapAsync(listingController.updateListing)
-);
-
-//delete route
-router.delete(
-    "/:id",
+    wrapAsync(listingController.updateListing))
+    .delete(
     isLoggedin,
     isOwner,
-    wrapAsync(listingController.destroyListing)
-);
+    wrapAsync(listingController.destroyListing));
+
+router
+    .route("/:id/edit")
+    .get(
+        isLoggedin,
+        isOwner,
+        wrapAsync(listingController.renderEditForm));
 
 module.exports = router;
