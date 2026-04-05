df85c18105363ac5fe563a2869c0e98cb9e4088c
Rakesh D <160116250+rakesh2OO5@users.noreply.github.com>
2026-04-06 01:51:13 +0530

    Update ownership checks in middleware functions

--- middleware.js
+++ middleware.js
@@ -23,7 +23,7 @@ module.exports.saveRedirectUrl = (req,res,next)=>{
 module.exports.isOwner =async (req,res,next)=>{
     let {id} = req.params;
     let listing =await Listing.findById(id);
-    if(!res.locals.currUser._id.equals(listing.owner)){
+  if (!req.user || !listing.owner.equals(req.user._id)){
         req.flash("error","Permission Denied");
         return res.redirect(`/listings/${id}`);
     }
@@ -32,7 +32,7 @@ module.exports.isOwner =async (req,res,next)=>{
 module.exports.isReviewAuthor =async (req,res,next)=>{
     let {id,reviewId} = req.params;
     let review =await Review.findById(reviewId);
-    if(!res.locals.currUser._id.equals(review.author)){
+   if (!req.user || !review.author.equals(req.user._id)) {
         req.flash("error","Permission Denied");
         return res.redirect(`/listings/${id}`);
     }
